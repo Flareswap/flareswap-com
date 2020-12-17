4e6253d89c4dc9d1d3259acfab0ef09962bce25e
webeli <pe_lias@msn.com>
2020-12-17 17:46:03 +0100

    Added react-router-dom and updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,27 @@
 import React from "react";
-import { PageHeader, Row, Button, Col } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Start from "./pages/Start";
+import Trade from "./pages/Trade";
 
 function App() {
   return (
-    <div>
-      <PageHeader
-        ghost={false}
-        title="Flareswap"
-        subTitle="- decentralized cryptocurrency exchange on the Flare Network."
-        extra={[
-          <Button key="1" type="text">
-            Community
-          </Button>,
-          <Button key="2" type="text">
-            About
-          </Button>,
-          <Button key="3" type="primary">
-            Launch App
-          </Button>,
-        ]}
-      ></PageHeader>
-      <Row justify="center" align="middle">
-        <Col className="flex flex--flex-direction--column flex--justify-content--center flex--align-items--center text--align--center margin--top--5 ">
-          <p>
-            Flareswap is a decentralized cryptocurrency exchange which
-            facilitates automated transactions between <br />
-            the Flare ecosystem and established cryptocurrencies
-            through the use of smart contracts.
-          </p>
-          <Button type="primary">Launch App</Button>
-        </Col>
-      </Row>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/trade">
+            <Trade />
+          </Route>
+          <Route path="/">
+            <Start />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
