8742baa3437f2e712a9f099fb6d676e4da83c9b6
webeli <pe_lias@msn.com>
2020-12-18 18:34:27 +0100

    Updated Trade page

--- src/pages/Trade/index.tsx
+++ src/pages/Trade/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import TradeCard from '../../components/TradeCard';
 
 export default function Trade() {
-  return <div>Trade</div>;
-};
+  return (
+    <div className="flex flex--flex-direction--column flex--justify-content--center flex--align-items--center text--align--center margin--top--5">
+      <TradeCard />
+    </div>
+  );
+}
