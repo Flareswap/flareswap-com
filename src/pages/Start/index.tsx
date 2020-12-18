523c6e8d174313ccbf573aa07051082c6313cd43
webeli <pe_lias@msn.com>
2020-12-18 18:34:47 +0100

    Updated Start

--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -1,19 +1,21 @@
 import { Button, Col, Row } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
+import { PoweroffOutlined, FireOutlined } from "@ant-design/icons";
 
 export default function Start() {
   return (
     <Row justify="center" align="middle">
-      <Col className="flex flex--flex-direction--column flex--justify-content--center flex--align-items--center text--align--center margin--top--5 ">
-        <p>
+      <Col className="flex flex--flex-direction--column flex--justify-content--center flex--align-items--center text--align--center margin--top--5">
+        <FireOutlined style={{ fontSize: 200, color: "#00C281" }} />
+        <p className="margin--top--4">
           Flareswap is a decentralized cryptocurrency exchange which facilitates
           automated transactions between <br />
           the Flare ecosystem and established cryptocurrencies through the use
           of smart contracts.
         </p>
         <Link to="/trade">
-          <Button key="3" type="primary">
+          <Button key="1" type="primary" icon={<PoweroffOutlined />}>
             Launch App
           </Button>
         </Link>
