d53deb2d803578dcd177c10c9a54aaa25d899c09
webeli <pe_lias@msn.com>
2020-12-20 00:38:56 +0100

    Added transition on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Layout from "./components/Layout";
+
 import About from "./pages/About";
 import Start from "./pages/Start";
 import Features from "./pages/Features";
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/features">
-            <Features />
-          </Route>
-          <Route path="/">
-            <Start />
-          </Route>
-        </Switch>
-      </Layout>
-    </Router>
+    <Layout>
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          classNames="fade"
+          timeout={{ enter: 300, exit: 0 }}
+        >
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/features">
+              <Features />
+            </Route>
+            <Route path="/">
+              <Start />
+            </Route>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </Layout>
   );
 }
